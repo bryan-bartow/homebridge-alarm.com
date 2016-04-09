b890bf056fbe28ac8f730df15c3c3fc779a47c4c
Bryan Bartow <bryanbartow@gmail.com>
2016-04-08 23:54:49 -0500

    Updated setState method to use WrapAPI

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ function AlarmcomAccessory(log, config) {
 
 AlarmcomAccessory.prototype.getState = function(callback) {
 
-  //initLogin(callback).bind(this);
   this.log('getting sessionUrl');
 
   request.get({
@@ -47,7 +46,7 @@ AlarmcomAccessory.prototype.getState = function(callback) {
       this.login(callback);
     }
     else {
-      this.log("Error getting state (status code %s): %s", response.statusCode, err);
+      this.log("Error getting sessionUrl (status code %s): %s", response.statusCode, err);
       callback(err);
     }
   }.bind(this));
@@ -115,7 +114,7 @@ AlarmcomAccessory.prototype.login = function(callback) {
       callback(null, statusResult.status);
     }
     else {
-      this.log("Error getting state (status code %s): %s", response.statusCode, err);
+      this.log("Error logging in (status code %s): %s", response.statusCode, err);
       callback(err);
     }
   }.bind(this));
@@ -123,91 +122,85 @@ AlarmcomAccessory.prototype.login = function(callback) {
 
 AlarmcomAccessory.prototype.setState = function(state, callback) {
 
-  var statusResult = new Object();
+  this.log('getting sessionUrl');
 
-	try {
+  request.get({
+    url: "https://wrapapi.com/use/bryanbartow/alarmdotcom/initlogin/0.0.2",
+    qs: { wrapAPIKey: this.apiKey }
+  }, function(err, response, body) {
 
-		var driver = new phantomjs.Driver();
+    if (!err && response.statusCode == 200) {
+      var json = JSON.parse(body);
+      this.sessionUrl = json.data.sessionUrl;
 
-		driver.get('https://www.alarm.com/pda/Default.aspx');
-		driver.findElement(By.name('ctl00$ContentPlaceHolder1$txtLogin')).sendKeys(this.username);
-		driver.findElement(By.name('ctl00$ContentPlaceHolder1$txtPassword')).sendKeys(this.password);
-		driver.findElement(By.name('ctl00$ContentPlaceHolder1$btnLogin')).click();
+      this.setAlarmState(state, callback);
+    }
+    else {
+      this.log("Error getting sessionUrl (status code %s): %s", response.statusCode, err);
+      callback(err);
+    }
+  }.bind(this));
+}
 
-    return driver.getTitle().then(function(title) {
+AlarmcomAccessory.prototype.setAlarmState = function(state, callback) {
 
-      console.log('Logged in to alarm.com');
-      console.log("attempting to set state::" + state);
+  this.log('setting state to ' + state);
 
-      // Determine the element to click based on state
+  var apiVerb = "";
 
-      var setStateElementId;
-      var confirmStateElemendId = null;
+  // Figure out which API to call
+  if(state === Characteristic.SecuritySystemTargetState.DISARM) {
+    apiVerb = "disarm";
+  }
+  else if(state === Characteristic.SecuritySystemTargetState.STAY_ARM) {
+    apiVerb = "armstay";
+  }
+  else if(state === Characteristic.SecuritySystemTargetState.AWAY_ARM) {
+    apiVerb = "armaway";
+  }
 
-      if(state === Characteristic.SecuritySystemTargetState.DISARM) {
+  request.get({
+    url: "https://wrapapi.com/use/bryanbartow/alarmdotcom/" + apiVerb + "/0.0.2",
+    qs: {
+      wrapAPIKey: this.apiKey,
+      sessionUrl: this.sessionUrl,
+      username: this.username,
+      password: this.password
+    }
+  }, function(err, response, body) {
 
-        setStateElementId = 'ctl00_phBody_butDisarm';
+    if (!err && response.statusCode == 200) {
+      var json = JSON.parse(body);
+      this.log(json);
+      var alarmState = json.data.alarmState;
+
+      var statusResult = new Object();
+
+      if(alarmState === "Disarmed") {
+        statusResult.message = "disarmed";
+        statusResult.status = Characteristic.SecuritySystemCurrentState.DISARMED;
+      } else if(alarmState === "Armed Stay") {
+        statusResult.message = "stay_armed";
+        statusResult.status = Characteristic.SecuritySystemCurrentState.STAY_ARM;
+      } else if(alarmState === "Armed Away") {
+        statusResult.message = "away_armed";
+        statusResult.status = Characteristic.SecuritySystemCurrentState.AWAY_ARM;
       }
-      else if(state === Characteristic.SecuritySystemTargetState.STAY_ARM) {
 
-        setStateElementId = 'ctl00_phBody_butArmStay';
-      }
-      else if(state === Characteristic.SecuritySystemTargetState.AWAY_ARM) {
+      statusResult.success = true;
 
-        setStateElementId = 'ctl00_phBody_butArmAway';
-      }
-
-      driver.findElement(By.id(setStateElementId)).then(function(statusElement) {
-
-        statusElement.click().then(function(clickedStatusElement) {
-
-            statusResult.message = "state set to  " + state;
-            statusResult.success = true;
-
-			if(state === Characteristic.SecuritySystemTargetState.DISARMED) {
-				statusResult.status = Characteristic.SecuritySystemCurrentState.DISARMED;
-			} else if(state === Characteristic.SecuritySystemTargetState.STAY_ARM) {
-				statusResult.status = Characteristic.SecuritySystemCurrentState.STAY_ARM;
-			} else {
-				statusResult.status = Characteristic.SecuritySystemCurrentState.AWAY_ARM;
-			}
-
-            driver.quit();
-
-            return statusResult;
-        });
-
-      }, function(error) {
-
-        console.log("Can't find state element");
-
-        statusResult.message = "Can't find the state element";
-        statusResult.success = false;
-
-        driver.quit();
-      });
-
-      return statusResult;
-
-		}, 1000).then(function(result) {
-
-      this.log(result);
+      this.log(statusResult);
 
       this.service
-        .setCharacteristic(Characteristic.SecuritySystemCurrentState, result.status);
+        .setCharacteristic(Characteristic.SecuritySystemCurrentState, statusResult.status);
 
-			callback(null);
-		}.bind(this));
-	}
-	catch(exception) {
-
-    statusResult.message = exception.message;
-    statusResult.success = false;
-
-		console.log(statusResult);
-
-		callback(statusResult);
-	}
+      callback(null, statusResult.status);
+    }
+    else {
+      this.log("Error getting state (status code %s): %s", response.statusCode, err);
+      callback(err);
+    }
+  }.bind(this));
 }
 
 AlarmcomAccessory.prototype.getServices = function() {
