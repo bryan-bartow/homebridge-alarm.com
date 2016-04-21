7f5f95a42bd2f8597bff24336e35ebf6aae444cb
Timothy Yung <yungsters@gmail.com>
2016-04-21 14:50:36 -0700

    Only send user credentials for `login`

--- index.js
+++ index.js
@@ -102,12 +102,12 @@ module.exports = homebridge => {
 
     login() {
       return this.send('initlogin').then(json => {
-        const sessionParams = {
-          sessionUrl: json.data.sessionUrl,
+        const sessionUrl = json.data.sessionUrl;
+        return this.send('login', {
+          sessionUrl,
           username: this.config.username,
           password: this.config.password,
-        };
-        return this.send('login', sessionParams).then(json => {
+        }).then(json => {
           switch (json.data.alarmState) {
             case 'Disarmed':
               return Characteristic.SecuritySystemCurrentState.DISARMED;
@@ -121,7 +121,7 @@ module.exports = homebridge => {
         }).then(currentState => {
           return {
             currentState,
-            send: action => this.send(action, sessionParams),
+            send: action => this.send(action, {sessionUrl}),
           };
         });
       });
